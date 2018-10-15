c9211397470a8ef6300ddea1b6b430b3dcaeaa3d
Greg <greg@Gregs-MacBook-Air.local>
2018-10-15 14:35:23 -0700

    algorithm questions tested

---
+++ algorithms.js
@@ -0,0 +1,39 @@
+//Code is all commented out so I could test it all in segments without overlapping output
+
+// for(var i=0; i<5; i++){
+//   console.log(i)
+// }
+
+// for(var i=0; i<5; i++){
+//   i = i+1;
+//   console.log(i);
+// }
+
+// for(var i =0; i < 5; i++){
+//   i = i+3;
+//   console.log(i);
+// }
+
+// function y(num1, num2){
+//   return num1 + num2;
+// };
+// console.log(y(2, 3));
+// console.log(y(3, 5));
+
+//  let a = 15;
+//  console.log(a);
+//  function y(a){
+//    console.log(a);
+//    return a;
+//  }
+// b = y(10);
+// console.log(b);
+
+// let a = 15;
+// console.log(a);
+// function y(a){
+//   console.log(a)
+//   return a*2
+// }
+// b= y(10);
+// console.log(b);
